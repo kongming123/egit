f3ab78853194f3e3992e5ef57843764b98e0f2be
kongming123 <jianxin_job@163,com>
2017-03-19 01:32:38 +0800

    update by kongming 01

--- src/com/atguigu/test/Hello.java
+++ src/com/atguigu/test/Hello.java
@@ -4,6 +4,10 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("hello world!");
+		System.out.println("Hello man!");
+		System.out.println("Hello woman!");
+		System.out.println("Hello neon!");
+		System.out.println("Hello kepler!");
 	}
 
 }
